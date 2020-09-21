a3c9b854b911c1f05cac9d453c34367c9ef980df
Melnyk <melnikdmitriy2002@gmail.com>
2020-09-21 21:03:55 +0300

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Task_1
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Привіт, ДонНУ!");
+            Console.ReadKey();
+        }
+    }
+}
